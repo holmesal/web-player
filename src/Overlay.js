a4bd3b5e59019e9c0f95562f7a2727e70908b738
Alonso Holmes <hello@alonso.io>
2016-01-04 22:39:05 -0600

    allowing touch events

--- src/Overlay.js
+++ src/Overlay.js
@@ -51,16 +51,16 @@ export default class Overlay extends React.Component {
 let style = {
     wrapper: {
         position: 'fixed',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
+        //top: 0,
+        //left: 0,
+        //bottom: 0,
+        //right: 0,
         //pointerEvents: 'none'
         //backgroundColor: 'rgba(22, 63, 14, 0.4)'
     },
     timeRow: {
         //backgroundColor: 'red',
-        position: 'absolute',
+        position: 'fixed',
         top: '50%',
         left: 0,
         right: 0,
@@ -74,11 +74,11 @@ let style = {
         fontWeight: 400
     },
     playButton: {
+        position: 'fixed',
         width: 106,
         height: 106,
         borderRadius: '50%',
         border: '1px solid #3e3e3e',
-        position: 'absolute',
         left: '50%',
         top: '50%',
         marginLeft: -53,
